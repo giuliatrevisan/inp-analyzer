e656897e3c881db7392b105dd389eada5edf1f13
giuliatrevisan <giulia.juca01@gmail.com>
2025-05-23 22:00:41 -0300

    corrigido app.py para deploy Render

--- app.py
+++ app.py
@@ -28,4 +28,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Para rodar localmente
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
